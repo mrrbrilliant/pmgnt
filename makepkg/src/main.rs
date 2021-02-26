2dac87ddc91acfac68c987acdd350d9fff4c8e84
mrrbrilliant <mrrbrilliantwork@gmail.com>
2021-02-26 12:48:55 +0700

    config path

--- makepkg/src/main.rs
+++ makepkg/src/main.rs
@@ -1,5 +1,26 @@
-use std::env;
+use lazy_static::*;
+use std::{path::{Path, PathBuf}, env};
+
+lazy_static! {
+    static ref BASEDIR: PathBuf = cwd();
+    static ref SRCDIR: PathBuf = cwd().join("source");
+    static ref PKGDIR: PathBuf = cwd().join("pkg");
+    static ref PKGFILE: PathBuf = cwd().join("pkgbuild.yml");
+}
+
+fn cwd() -> PathBuf {
+    if cfg!(debug_assertions) {
+        env::current_dir().unwrap().join("work")
+    } else {
+        env::current_dir().unwrap()
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}", BASEDIR.display());
+    println!("{}", BASEDIR.display());
+    println!("{}", BASEDIR.display());
+    println!("{}", BASEDIR.display());
 }
+
+fn config() {}
